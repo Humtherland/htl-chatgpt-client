680ca9eb706e53af86621ecf1ea60425be9376eb
samuel14luis <luismendoza.samuel@gmail.com>
2023-05-10 00:06:51 -0500

    chore: scaffolding file module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    FileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
